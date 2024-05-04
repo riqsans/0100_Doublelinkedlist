18e6d25bfe2038a487cde9910dbbedcc526c770e
riqsans <thrq88@gmail.com>
2024-05-05 06:47:01 +0700

    menambahkan algortima input

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -73,5 +73,10 @@ bool search(int rollNo, Node** previous, Node** current) {
 }
 
 void deleteNode() {
+    Node* previous, * current;
+	int rollNo;
+
+	cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+	cin >> rollNo;	// step 3: get the roll number to be deleted
 
 }
